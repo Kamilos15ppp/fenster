be37e8bc1d61b8244b592967674428caae6a5d1e
Kamil Sarga <kamil.sarga@live.com>
2021-08-17 15:55:16 +0200

    added font and breakpoints

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,14 @@
+const size = {
+    xs: '360px',
+    sm: '768px',
+    lg: '1200',
+}
+
+const device = {
+    xs: `(min-width: ${size.xs})`,
+    sm: `(min-width: ${size.sm})`,
+    lg: `(min-width: ${size.lg})`,
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { size, device }
