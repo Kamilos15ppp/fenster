4f9498f28be237b847a5b679b81c4e4001b91d77
Kamil Sarga <kamil.sarga@live.com>
2021-08-18 12:41:31 +0200

    added About Us page and translations

---
+++ src/pages/About/About.css.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const AboutWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const AboutHeader = styled.h1`
+    font-size: 2rem;
+    margin-bottom: 3rem;
+    text-align: center;
+`;
+
+export const AboutInfo = styled.article`
+    padding: 0 2.4rem;
+    margin-bottom: 3rem;
+    font-size: 1.2rem;
+    text-align: justify;
+`;
+
+export const AboutLogotype = styled.img`
+    margin: 2rem 0;
+    width: 70%;
+`;
+
+export const AboutSignet = styled.img`
+    margin: 2rem 0;
+    width: 30%;
+`;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useTranslation } from 'react-i18next';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import {
+    AboutWrapper,
+    AboutHeader,
+    AboutInfo,
+    AboutLogotype,
+    AboutSignet
+} from './About.css';
+
+import logotype from '../../assets/images/logotype_small.png';
+import signet from '../../assets/images/signet_small.png';
 
 function About() {
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
-        <div>
-            About
-        </div>
+        <AboutWrapper>
+            <AboutHeader
+                data-aos='fade-down'
+                data-aos-delay="300"
+                data-aos-duration="1000"
+            >
+                {t('about')}
+            </AboutHeader>
+            <AboutInfo
+                data-aos='fade-up-left'
+                data-aos-delay="800"
+                data-aos-duration="1000"
+            >
+                {t('about_info')}
+            </AboutInfo>
+            <AboutLogotype
+                data-aos='zoom-in'
+                data-aos-duration="1000"
+                src={logotype}
+            />
+            <AboutSignet
+                data-aos='zoom-in'
+                data-aos-delay="300"
+                data-aos-duration="1000"
+                src={signet}
+            />
+        </AboutWrapper>
     );
 }
 
