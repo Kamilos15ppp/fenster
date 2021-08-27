866012f102e3924b1e4e9ac52968a54eff762326
Kamil Sarga <kamil.sarga@live.com>
2021-08-27 21:16:13 +0200

    changed images and animations

--- src/pages/Gallery/Gallery.css.js
+++ src/pages/Gallery/Gallery.css.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 import breakpoints from "../../utils/breakpoints";
 
-import img1 from '../../assets/images/1.jpg';
-import img2 from '../../assets/images/2.jpg';
-import img3 from '../../assets/images/3.jpg';
-
-const images = [img1, img2, img3];
-
 export const GalleryWrapper = styled.section`
     display: flex;
     flex-direction: column;
@@ -24,6 +18,7 @@ export const ImagesWrapper = styled.div`
     grid-template-columns: 1fr;
     grid-template-rows: repeat(6, 1fr);
     grid-gap: 2rem;
+    padding: 3rem;
 
 		@media only screen and ${breakpoints.device.sm} {
 		  	margin-top: 2rem;
@@ -39,26 +34,8 @@ export const ImagesWrapper = styled.div`
 		}
 `;
 
-export const GalleryImage = styled.div`
-    background-image: ${props => props.num ? `url(${images[props.num]})` : null};
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    place-self: center;
-    margin: 2rem 0;
-    width: 17rem;
-    height: 13rem;
-    border-radius: 15px;
-	box-shadow: 2px 2px 15px 0 #000;
-
-	@media only screen and ${breakpoints.device.sm} {
-	  	margin: 0;
-      	width: 20rem;
-      	height: 16rem;
-	}
-  
-	@media only screen and ${breakpoints.device.lg} {
-      	width: 23rem;
-      	height: 19rem;
-	}
+export const GalleryImg = styled.img`
+	width: 90%;
+	border-radius: 15px;
+	place-self: center;
 `;
--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,20 +1,52 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import { useTranslation } from 'react-i18next';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { gsap } from 'gsap';
 
 import {
     GalleryWrapper,
     GalleryHeader,
-    GalleryImage,
+    GalleryImg,
     ImagesWrapper
 } from './Gallery.css';
 
+import image1Small from '../../assets/compressedimgs/window_640.jpg';
+import image1Medium from '../../assets/compressedimgs/window_1280.jpg';
+import image1Large from '../../assets/compressedimgs/window_1920.jpg';
+import image2Small from '../../assets/compressedimgs/windows_640.jpg';
+import image2Medium from '../../assets/compressedimgs/windows_1280.jpg';
+import image2Large from '../../assets/compressedimgs/windows_1920.jpg';
+import image3Small from '../../assets/compressedimgs/window-transparent_640.jpg';
+import image3Medium from '../../assets/compressedimgs/window-transparent_1280.jpg';
+import image3Large from '../../assets/compressedimgs/window-transparent_1920.jpg';
+import image4Small from '../../assets/compressedimgs/window-transparent-wood_640.jpg';
+import image4Medium from '../../assets/compressedimgs/window-transparent-wood_1280.jpg';
+import image4Large from '../../assets/compressedimgs/window-transparent-wood_1920.jpg';
+import image5Small from '../../assets/compressedimgs/people_640.jpg';
+import image5Medium from '../../assets/compressedimgs/people_1280.jpg';
+import image5Large from '../../assets/compressedimgs/people_1920.jpg';
+import image6Small from '../../assets/compressedimgs/chairs_640.jpg';
+import image6Medium from '../../assets/compressedimgs/chairs_1280.jpg';
+import image6Large from '../../assets/compressedimgs/chairs_1920.jpg';
+
 function Gallery() {
     const { t } = useTranslation();
+    const sizes = '(max-width: 360px) 300px, (max-width: 768px) 640px, (max-width: 1300px) 1280px, 1920px';
+    const imagesRef = useRef(null);
 
     useEffect(() => {
         AOS.init();
+    }, []);
+
+    useEffect(() => {
+        const images = imagesRef.current.children;
+        gsap.fromTo(images[0], { scale: 0, autoAlpha: 0 }, { delay: .5, duration: 1.5, scale: 1, autoAlpha: 1 });
+        gsap.fromTo(images[1], { scale: 0, autoAlpha: 0 }, { delay: 1, duration: 1.5, scale: 1, autoAlpha: 1 });
+        gsap.fromTo(images[2], { scale: 0, autoAlpha: 0 }, { delay: 1.5, duration: 1.5, scale: 1, autoAlpha: 1 });
+        gsap.fromTo(images[3], { scale: 0, autoAlpha: 0 }, { delay: 2, duration: 1.5, scale: 1, autoAlpha: 1 });
+        gsap.fromTo(images[4], { scale: 0, autoAlpha: 0 }, { delay: 2.5, duration: 1.5, scale: 1, autoAlpha: 1 });
+        gsap.fromTo(images[5], { scale: 0, autoAlpha: 0 }, { delay: 3, duration: 1.5, scale: 1, autoAlpha: 1 });
     }, []);
 
     return (
@@ -27,48 +59,48 @@ function Gallery() {
             >
                 {t('gallery')}
             </GalleryHeader>
-            <ImagesWrapper>
-                <GalleryImage
-                    num={'0'}
-                    data-aos='zoom-in'
-                    data-aos-delay="500"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
+            <ImagesWrapper ref={imagesRef}>
+                <GalleryImg
+                    srcSet={`${image1Small} 640w, ${image1Medium} 1280w, ${image1Large} 1920w`}
+                    sizes={sizes}
+                    src={image1Large}
+                    alt='image1'
+                    loading='lazy'
                 />
-                <GalleryImage
-                    num={'1'}
-                    data-aos='zoom-in'
-                    data-aos-delay="700"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
+                <GalleryImg
+                    srcSet={`${image2Small} 640w, ${image2Medium} 1280w, ${image2Large} 1920w`}
+                    sizes={sizes}
+                    src={image2Large}
+                    alt='image2'
+                    loading='lazy'
                 />
-                <GalleryImage
-                    num={'2'}
-                    data-aos='zoom-in'
-                    data-aos-delay="800"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
+                <GalleryImg
+                    srcSet={`${image3Small} 640w, ${image3Medium} 1280w, ${image3Large} 1920w`}
+                    sizes={sizes}
+                    src={image3Large}
+                    alt='image3'
+                    loading='lazy'
                 />
-                <GalleryImage
-                    num={'0'}
-                    data-aos='zoom-in'
-                    data-aos-delay="900"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
+                <GalleryImg
+                    srcSet={`${image4Small} 640w, ${image4Medium} 1280w, ${image4Large} 1920w`}
+                    sizes={sizes}
+                    src={image4Large}
+                    alt='image4'
+                    loading='lazy'
                 />
-                <GalleryImage
-                    num={'1'}
-                    data-aos='zoom-in'
-                    data-aos-delay="1000"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
+                <GalleryImg
+                    srcSet={`${image5Small} 640w, ${image5Medium} 1280w, ${image5Large} 1920w`}
+                    sizes={sizes}
+                    src={image5Large}
+                    alt='image5'
+                    loading='lazy'
                 />
-                <GalleryImage
-                    num={'2'}
-                    data-aos='zoom-in'
-                    data-aos-delay="1100"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
+                <GalleryImg
+                    srcSet={`${image6Small} 640w, ${image6Medium} 1280w, ${image6Large} 1920w`}
+                    sizes={sizes}
+                    src={image6Large}
+                    alt='image6'
+                    loading='lazy'
                 />
             </ImagesWrapper>
         </GalleryWrapper>
